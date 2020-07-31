07c40eb5c1ebf592bea04f196a0f2ca8e56552f6
nagamiya <appngmy@gmail.com>
2020-08-01 00:12:02 +0900

    adminページでuser,postの編集ができるようにした

--- floattle/admin.py
+++ floattle/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Post
 
 # Register your models here.
+
+# Authority: make user
+admin.site.register(User)
+# Authority: make post, Edit post...
+admin.site.register(Post)
