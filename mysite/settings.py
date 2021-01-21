75fd3c7e587861fd2efc76b66ad3066454e98b6c
nagamiya <appngmy@gmail.com>
2021-01-22 04:09:54 +0900

    settings.py edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 from socket import gethostname
 import dj_database_url
 import os
@@ -225,3 +225,6 @@ else:
         'default': dj_database_url.config()
     }
     ALLOWED_HOSTS = ['*']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
