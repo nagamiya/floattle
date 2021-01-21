ef31f7506a2d9191ceda72c60fcc2f23d556fe5a
nagamiya <appngmy@gmail.com>
2021-01-22 05:04:48 +0900

    a

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,4 +7,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
